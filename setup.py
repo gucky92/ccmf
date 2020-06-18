de982d15edd39abcb914c8ad39eedfd763726160
Tommy <kclamar@connect.ust.hk>
2020-06-17 22:05:04 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -11,5 +11,16 @@ setup(
     author='Ka Chung Lam',
     author_email='kclamar@connect.ust.hk',
     description='Circuit-constrained matrix factorization',
-    install_requires=['networkx', 'pandas', 'numpy', 'pyro-ppl', 'scikit-learn', 'tqdm', 'torch']
+    install_requires=['networkx', 'pandas', 'numpy', 'pyro-ppl', 'scikit-learn', 'tqdm', 'torch'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+      ],
+    download_url='https://github.com/kclamar/ccmf/archive/v0.0.1.tar.gz'
 )
