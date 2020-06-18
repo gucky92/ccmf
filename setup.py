8e08e578a3c6297fcadeda0d5f5917518438eeff
Tommy <kclamar@connect.ust.hk>
2020-06-17 21:58:23 -0400

    Added LICENSE

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name='ccmf',
-    version='',
+    version='0.0.1',
+    python_requires='>=3.5',
     packages=['ccmf', 'ccmf.gui', 'ccmf.gui.tk', 'ccmf.gui.tk.gui_circuit', 'ccmf.ccmf', 'ccmf.model',
               'ccmf.model.base', 'ccmf.model.linear', 'ccmf.circuit', 'ccmf.dataset', 'ccmf.inference'],
     url='https://github.com/kclamar/ccmf',
-    license='',
+    license='MIT license',
     author='Ka Chung Lam',
-    author_email='',
+    author_email='kclamar@connect.ust.hk',
     description='Circuit-constrained matrix factorization',
     install_requires=['networkx', 'pandas', 'numpy', 'pyro-ppl', 'scikit-learn', 'tqdm', 'torch']
 )
